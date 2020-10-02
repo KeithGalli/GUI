2cc87a78b00c6c331b4ed31a13d3b17835b3ed90
10doshi12 <72262399+10doshi12@users.noreply.github.com>
2020-10-02 19:16:18 +0530

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import requests
 
 HEIGHT = 500
@@ -48,7 +49,8 @@ frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 entry = tk.Entry(frame, font=40)
 entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
+# ttk buttoms look more cool
+button = ttk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
