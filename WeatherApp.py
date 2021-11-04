ec453533f302715ab6b07f8a665046c5316867db
Jeevan Alexen <auracodehandle@gmail.com>
2021-11-04 12:01:52 +0530

    Corrected parameters of font to what was specified in the video

--- WeatherApp.py
+++ WeatherApp.py
@@ -45,16 +45,16 @@ background_label.place(relwidth=1, relheight=1)
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
-entry = tk.Entry(frame, font=40)
+entry = tk.Entry(frame, font=('Courier, 18'))
 entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
+button = tk.Button(frame, text="Get Weather", font=('Courier, 12'), command=lambda: get_weather(entry.get()))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
-label = tk.Label(lower_frame)
+label = tk.Label(lower_frame,font=('Courier, 18'))
 label.place(relwidth=1, relheight=1)
 
 root.mainloop()
